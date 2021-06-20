c133864488a4affc9309e4dd04287df313dd6c25
NILOIDE <nil@medis.nl>
2021-06-20 18:48:56 +0200

    Added max ims

--- extract_video_frames.py
+++ extract_video_frames.py
@@ -25,7 +25,7 @@ if __name__=="__main__":
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video", default=r"C:\Users\Nil\Downloads\Alternate Realities.mp4")
     a.add_argument("--pathOut", help="path to images", default=r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\Alternate_realities_2400")
-    a.add_argument("--numIms", help="number of images to extract", default=0)
+    a.add_argument("--numIms", help="number of images to extract", type=int, default=0)
     args = a.parse_args()
     print(args)
     extractImages(args.pathIn, args.pathOut, args.numIms)
