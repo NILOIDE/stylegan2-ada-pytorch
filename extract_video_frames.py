176145e830b3bda87852f37a6e4a8e1b00f48530
NILOIDE <nil@medis.nl>
2021-06-20 16:44:17 +0200

    Added max ims

--- extract_video_frames.py
+++ extract_video_frames.py
@@ -5,7 +5,7 @@ import cv2
 print(cv2.__version__)
 
 
-def extractImages(pathIn, pathOut):
+def extractImages(pathIn, pathOut, numIms):
     count = 0
     vidcap = cv2.VideoCapture(pathIn)
     success, image = vidcap.read()
@@ -17,12 +17,15 @@ def extractImages(pathIn, pathOut):
         print('Read a new frame: ', success, f'Saving as:  {save_path}')
         cv2.imwrite(save_path, image)     # save frame
         count = count + 1
+        if numIms and count > numIms:
+            break
 
 
 if __name__=="__main__":
     a = argparse.ArgumentParser()
     a.add_argument("--pathIn", help="path to video", default=r"C:\Users\Nil\Downloads\Alternate Realities.mp4")
     a.add_argument("--pathOut", help="path to images", default=r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\Alternate_realities_2400")
+    a.add_argument("--numIms", help="number of images to extract", default=0)
     args = a.parse_args()
     print(args)
-    extractImages(args.pathIn, args.pathOut)
+    extractImages(args.pathIn, args.pathOut, args.numIms)
