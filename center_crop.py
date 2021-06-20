1a65bf90f4f0fb7a9980c0a3455c73d14596ef94
NILOIDE <nil@medis.nl>
2021-06-20 19:47:57 +0200

    Updated image crop script

--- center_crop.py
+++ center_crop.py
@@ -3,15 +3,16 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 import os
+import argparse
 
-DATASET_DIR = Path(r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\ImaginaryLandscapes_top")
-DATA_SAVE_DIR = Path(r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\ImaginaryLandscapes_top\256")
+DATASET_DIR = r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\ImaginaryLandscapes_top"
+DATA_SAVE_DIR = r"C:\Users\Nil\PycharmProjects\StyleGAN2\data\ImaginaryLandscapes_top\256"
 
 
-def main(resolution=256, inter=cv2.INTER_AREA):
-    os.makedirs(str(DATA_SAVE_DIR), exist_ok=True)
-    for filename in tqdm(os.listdir(DATASET_DIR)):
-        img = cv2.imread(str(DATASET_DIR / filename))
+def main(dataset_dir, data_save_dir, resolution, inter=cv2.INTER_AREA):
+    os.makedirs(str(data_save_dir), exist_ok=True)
+    for filename in tqdm(os.listdir(dataset_dir)):
+        img = cv2.imread(str(dataset_dir / filename))
         if img is not None:
             assert img.shape[0] > 3
             (h, w) = img.shape[:2]
@@ -27,8 +28,13 @@ def main(resolution=256, inter=cv2.INTER_AREA):
                                      crop_origin[1]:crop_origin[1]+resolution]
             assert cropped_im.shape == (resolution, resolution, 3),\
                 f"Expected {(resolution, resolution, 3)}, received {cropped_im.shape}"
-            cv2.imwrite(str(DATA_SAVE_DIR / filename), cropped_im)
+            cv2.imwrite(str(data_save_dir / filename), cropped_im)
 
 
 if __name__ == '__main__':
-    main()
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathIn", help="path to video", type=str, default=DATASET_DIR)
+    a.add_argument("--pathOut", help="path to images", type=str, default=DATA_SAVE_DIR)
+    a.add_argument("--resolution", help="number of images to extract", type=int, default=256)
+    args = a.parse_args()
+    main(Path(args.pathIn), Path(args.pathOut), args.resolution)
